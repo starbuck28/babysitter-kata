f59a4f6381bbd109b4672526156f1f1e0ec5ead4
Elaine Oglesbay <elaine.oglesbay@gmail.com>
2016-08-27 12:57:44 -0400

    first attempt at a test driven function

--- js/sitter.js
+++ js/sitter.js
@@ -1,3 +1,23 @@
+//Default start time
 var startTime;
+//Default end time
 var endTime;
+//Default bedtime
 var bedtime;
+//Pay for entire shift
+var shiftPay;
+
+//Pay rate from 5pm to bedtime
+var earlyRate = 12;
+//Pay rate from bedtime to 12am
+var midRate = 8;
+//Pay rate from 12 am to 4 am
+var lateRate = 16;
+
+function getEarlyPay(startTime, bedtime, earlyRate) {
+	
+	var earlyPay = (bedtime - startTime) * earlyRate;
+	return earlyPay;
+}
+
+
--- tests/tests.js
+++ tests/tests.js
@@ -8,6 +8,18 @@ QUnit.test("sample test", function(assert) {
 assert.ok("1" == 1, "passed");
 });
 
-QUnit.test("startTime exists", function(assert) {
-  assert.ok(startTime, "startTime exists");
+QUnit.test("early rate", function(assert) {
+	assert.ok(earlyRate === 12, "early rate");
+});
+
+QUnit.test("mid rate", function(assert) {
+	assert.ok(midRate === 8, "mid rate");
+});
+
+QUnit.test("late rate", function(assert) {
+	assert.ok(lateRate === 16, "late rate");
+});
+
+QUnit.test("calculate pay with early rate", function(assert) {
+	assert.equal(getEarlyPay(7, 9, 12), 24, "Early pay rate is " + 24)
 });
